22ce4ff45e5b574f231b9445c0caca6f843e055e
rossitech <kelseylevine@gmail.com>
2022-12-09 13:40:43 -0500

    new mungo file

--- Assets/Mungo.cs
+++ Assets/Mungo.cs
@@ -10,6 +10,21 @@ public class Mungo : MonoBehaviour
     int walk_velocity;
     private float horizVelocity;
     private bool dubJump;
+
+    // Fields used for inventory
+    [SerializeField] private UI_Inventory uiInventory;
+    private Inventory inventory;
+    
+    // can be level 1,2,3
+    public int level = 1;
+
+    private void Awake()
+    {
+        inventory = new Inventory();
+        inventory.level = level;
+        uiInventory.SetInventory(inventory);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +37,11 @@ public class Mungo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // display the inventory
+        if (Input.GetKeyDown("tab")) {
+            ShowMungoInventory();
+        }
+
         if(!has_won){
 
             horizVelocity += (float)(Time.deltaTime * -9.81);
@@ -81,6 +101,44 @@ public class Mungo : MonoBehaviour
 
             cc.Move(movement_direction * Time.deltaTime * velocity);
 
+        }
+    }
+    
+    public void AddItemToInventory(string itemName) {
+        switch (itemName) {
+            default:
+            case "Apple":         
+                inventory.AddItem(new Item { itemType = Item.ItemType.Apple, amount = 1, level = level });
+                break;
+            case "Eggs":         
+                inventory.AddItem(new Item { itemType = Item.ItemType.Eggs, amount = 1, level = level });
+                break;
+            case "Cake":         
+                inventory.AddItem(new Item { itemType = Item.ItemType.Cake, amount = 1, level = level });
+                break;
+            case "Avacado":         
+                inventory.AddItem(new Item { itemType = Item.ItemType.Avacado, amount = 1, level = level });
+                break;
+            case "Strawberry":         
+                inventory.AddItem(new Item { itemType = Item.ItemType.Strawberry, amount = 1, level = level });
+                break;
+            case "PurpleFood":    
+                inventory.AddItem(new Item { itemType = Item.ItemType.PurpleFood, amount = 1, level = level });
+                break;
+        }
+        uiInventory.RefreshInventoryItems();
+    }
+
+    public void RemoveItemFromInventory(Item item) {
+        inventory.RemoveItem(item);
+        uiInventory.RefreshInventoryItems();
+    }
+
+    public void ShowMungoInventory()  {
+        Animator animator = uiInventory.GetComponent<Animator>();
+        if(animator != null) {
+            bool isShowing = animator.GetBool("show");
+            animator.SetBool("show", !isShowing);
         }
     }
 }
