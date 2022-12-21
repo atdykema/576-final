6ce2036744879541f1635ab85fbdc0abd6ce7bab
Andrew Dykema <62975486+atdykema@users.noreply.github.com>
2022-12-21 12:06:33 -0500

    evilnpc code + fix some animations

--- Assets/EvilNpc.cs
+++ Assets/EvilNpc.cs
@@ -12,12 +12,15 @@ public class EvilNpc : MonoBehaviour
 
     private NavMeshAgent agent;
 
+    int curr_update;
+
     bool has_won;
     // Start is called before the first frame update
     void Start()
     {
         has_won = false;
         agent = GetComponent<NavMeshAgent>();
+        curr_update = 0;
     }
 
     // Update is called once per frame
@@ -32,18 +35,28 @@ public class EvilNpc : MonoBehaviour
         }
 
         if(!has_won){
-            float dist = Vector3.Distance(this.transform.position, player.transform.position);
 
+            float dist = Vector3.Distance(this.transform.position, player.transform.position);
+            
             if(dist <= 100.0f){
                 agent.speed = 9000;
+                evilNpc.SetDestination(player.position);
             }else if(dist <= 1000.0f){
                 agent.speed = 7500;
+                evilNpc.SetDestination(player.position);
             }else if(dist <= 2500.0f){
                 agent.speed = 2500;
+                evilNpc.SetDestination(player.position);
             }else{
-                agent.speed = 0;
+                if(curr_update >= 10000){
+                    curr_update = 0;
+                    agent.speed = 1000;
+                    int randX = Random.Range(1,500);
+                    int randZ = Random.Range(1,500);
+                    evilNpc.SetDestination(new Vector3(this.transform.position.x + randX, this.transform.position.z + randZ));
+                }
+                curr_update++;
             }
-            evilNpc.SetDestination(player.position); 
         }else{
             evilNpc.SetDestination(this.transform.position);
         }
